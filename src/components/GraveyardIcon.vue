5e7ac41457446365cfecb231265a099af2299bee
Megajess <megajess@gmail.com>
2026-02-28 14:47:00 -0500

    Update font to give old school terminal vibe

--- src/components/GraveyardIcon.vue
+++ src/components/GraveyardIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg viewBox="0 0 100 140" xmlns="http://www.w3.org/2000/svg">
         <path d="M 50 20 Q 30 20 30 40 L 30 100 L 70 100 L 70 40 Q 70 20 50 20 Z" fill="#95d5b2" />
-        <text x="50" y="75" text-anchor="middle" fill="#1b4332" font-size="24">RIP</text>
+        <text x="50" y="65" text-anchor="middle" fill="#1b4332" font-size="12">RIP</text>
     </svg>
 </template>
