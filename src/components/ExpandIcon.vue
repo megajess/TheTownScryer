491399b69d9d6bee47fc3c59a124f7827967f0fa
Megajess <megajess@gmail.com>
2026-03-01 13:32:01 -0500

    Updating the collapse and expand icons

--- src/components/ExpandIcon.vue
+++ src/components/ExpandIcon.vue
@@ -1,9 +1,7 @@
 <template>
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M 10,4 L 21,4 L 21,21 L 4,21 L 4,10" stroke="#95d5b2" stroke-width="1.5" fill="none"
+        <line x1="20" y1="20" x2="4" y2="4" stroke="#95d5b2" stroke-width="1.5" stroke-linecap="round" />
+        <polyline points="12,4 4,4 4,12" stroke="#95d5b2" stroke-width="1.5" fill="none"
             stroke-linecap="round" stroke-linejoin="round" />
-        <polyline points="12,7 7,7 7,12" stroke="#95d5b2" stroke-width="1.5" fill="none" stroke-linecap="round"
-            stroke-linejoin="round" />
-        <line x1="13" y1="13" x2="7" y2="7" stroke="#95d5b2" stroke-width="1.5" stroke-linecap="round" />
     </svg>
 </template>
